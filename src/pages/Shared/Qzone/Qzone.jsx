c46c4973eba5e5def82f58e3be6a069c87310b16
Sadman Sakib <sadmansakib123450@gmail.com>
2023-05-01 00:12:17 +0200

    right side nav done

---
+++ src/pages/Shared/Qzone/Qzone.jsx
@@ -0,0 +1,18 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import qzone1 from '../../../assets/qZone1.png'
+import qzone2 from '../../../assets/qZone2.png'
+import qzone3 from '../../../assets/qZone3.png'
+
+const Qzone = () => {
+    return (
+        <div className='bg-secondary text-center bg-light my-4 py-4 '>
+            <h4>Q-Zone</h4>
+            <img src={qzone1} alt="" />
+            <img src={qzone2} alt="" />
+            <img src={qzone3} alt="" />
+        </div>
+    );
+};
+
+export default Qzone;
--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,12 +1,34 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React from "react";
+import { Button, ListGroup } from "react-bootstrap";
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
+import Qzone from "../Qzone/Qzone";
+import bg from '../../../assets/bg.png'
 
 const RightNav = () => {
-    return (
-        <div>
-            <h2>right nav</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h4>Login With</h4>
+      <Button className="mb-2" variant="outline-primary">
+        
+        <FaGoogle/> Login With Google
+      </Button>
+      <Button variant="outline-secondary"> <FaGithub/> Login With Github</Button>
+      <div>
+      <ListGroup>
+      <ListGroup.Item> <FaFacebook/> Cras justo odio</ListGroup.Item>
+      <ListGroup.Item> <FaTwitter/> Dapibus ac facilisis in</ListGroup.Item>
+      <ListGroup.Item> <FaInstagram/> Morbi leo risus</ListGroup.Item>
+      
+    </ListGroup>
+      </div>
+      <Qzone></Qzone>
+      <div className="w-auto">
+        <img className="w-auto"  src={bg} alt="" />
+      </div>
+    </div>
+    
+  );
 };
 
 export default RightNav;
